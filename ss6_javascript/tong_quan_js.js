5b3e046daabdb502181e331f0ae6bc051ea10f60
DoanNhatLong <longs3515@gmail.com>
2025-10-06 09:52:34 +0700

    Bai_tap_form_table

---
+++ ss6_javascript/tong_quan_js.js
@@ -0,0 +1,12 @@
+function test() {
+    document.write("Hello");
+}
+function avg() {
+
+    var a = parseFloat(prompt("Nhập a"));
+    var b = parseFloat(prompt("Nhập b"));
+    var c = parseFloat(prompt("Nhập c"));
+    var d= (a+b+c)/3;
+    document.write(d);
+
+}
